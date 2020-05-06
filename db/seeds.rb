e943cbee66f39b8ce3b8e57e3e6904d50869b0b7
Patrick Ott <patrickott@gmx.de>
2020-05-06 14:38:44 +0200

    Seed Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+  dishoom = { name: "Dishoom", address: "London", phone_number: "090", category: "vegie" }
+  pizza = { name: "pizza_east", address: "Rom", phone_number: "0009", category: "italian" }
+  steakhouse = { name: "Steak", address: "rom", phone_number: "090", category: "steak" }
+  dubai = { name: "Dubai Castle", address: "Dubai", phone_number: "000900", category: "arabic" }
+  vino = { name: "Le vino", address: "Paris", phone_number: "08890", category: "french" }
+
+[dishoom, pizza, steakhouse, dubai, vino].each do |attributes|
+  puts attributes
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
+
